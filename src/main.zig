d85fa9443605da5dcefa54efc615cb0df5deb8fb
danielaguiar0006 <danielaguiar0006@gmail.com>
2024-10-31 23:50:37 -0700

    Add: /echo/{str} Endpoint & Custom Headers
    - Implemented /echo/<your string> endpoint to echo back the provided
    string.
    - Added custom headers Content-Type and Echo-Length to responses.
    - Enhanced error handling with proper HTTP status codes (405 Method Not
    Allowed, 404 Not Found).

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,5 @@
 //! A simple HTTP/1.1 server in Zig.
-//!
-//! This program listens on port 9090 and responds with "Hello http!" to any request.
-//! It also responds with "OK" to the root path ("/") and "NOT FOUND" for any other request.
+//! This program echoes back the request body when there is a GET request for: "/echo/<your string>".
 
 const std = @import("std");
 
@@ -14,7 +12,7 @@ pub fn main() !void {
         return err;
     };
 
-    var server = endpoint.listen(.{}) catch |err| {
+    var server = endpoint.listen(.{ .reuse_address = true }) catch |err| {
         std.debug.print("Error listening on port :{}\ndue to : {}", .{ port, err });
         return err;
     };
@@ -42,24 +40,52 @@ fn startServer(server: *std.net.Server) !void {
 
         var request = http_server.receiveHead() catch |err| {
             std.debug.print("Error receiving request: {}\n", .{err});
+            try connection.stream.writeAll("HTTP/1.1 400 Bad Request\r\n\r\n");
+            connection.stream.close();
             continue;
         };
         handleRequest(&request) catch |err| {
             std.debug.print("Error handling request: {}\n", .{err});
+            try connection.stream.writeAll("HTTP/1.1 500 Internal Server Error\r\n\r\n");
+            connection.stream.close();
             continue;
         };
     }
 }
 
 /// Handles a request and sends a response back to the client.
-/// If the request is for the root path ("/"), responds with "OK" and a 200 status code.
-/// Otherwise, responds with "NOT FOUND" and a 404 status code.
 fn handleRequest(request: *std.http.Server.Request) !void {
     std.debug.print("Handling request for {s}\n", .{request.head.target});
 
-    if (std.mem.eql(u8, request.head.target, "/") and request.head.method == .GET) {
+    // Respond with "NOT GET" and a 404 status code when the request is not a GET request
+    if (request.head.method != .GET) {
+        try request.respond("METHOD NOT ALLOWED\n", .{ .status = .method_not_allowed });
+        return;
+    }
+
+    // Respond with "OK" and a 200 status code when the request is for the root path ("/")
+    if (std.mem.eql(u8, request.head.target, "/")) {
         try request.respond("OK\n", .{ .status = .ok });
-    } else {
+    }
+    // Echo back the request body, with a 200 status code and two response headers
+    else if (request.head.target.len > 6 and std.mem.eql(u8, request.head.target[0..6], "/echo/")) {
+        // Get the what was received in the request body
+        const echo = request.head.target[6..];
+        var echo_len_buffer: [16]u8 = undefined;
+        const echo_len_str = try std.fmt.bufPrint(&echo_len_buffer, "{d}", .{echo.len});
+
+        // Create additional headers
+        const headers = [_]std.http.Header{
+            .{ .name = "Content-Type", .value = "text/plain" },
+            .{ .name = "Echo-Length", .value = echo_len_str },
+        };
+
+        try request.respond(echo, .{
+            // NOTE: Zig's std.http.Server.Request.respond function automatically adds a content-length header
+            .status = .ok,
+            .extra_headers = &headers,
+        });
+    } else { // Respond with "NOT FOUND" and a 404 status code for any other request
         try request.respond("NOT FOUND\n", .{ .status = .not_found });
     }
 }
